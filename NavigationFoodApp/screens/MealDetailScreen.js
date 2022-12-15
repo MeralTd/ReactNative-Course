475077e33154db743e9462d8d261c9a37445a732
Meral <meraltasdemir77@gmail.com>
2022-12-15 17:00:16 +0300

    add headerRight button

--- NavigationFoodApp/screens/MealDetailScreen.js
+++ NavigationFoodApp/screens/MealDetailScreen.js
@@ -1,14 +1,29 @@
+import { useLayoutEffect } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import IconButton from '../components/IconButton';
 
 import List from '../components/MealDetail/List';
 import Subtitle from '../components/MealDetail/Subtitle';
 import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data';
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPressHandler() {
+    console.log('Pressed!');
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <IconButton icon="star" color="white" onPress={headerButtonPressHandler} />
+      }
+    });
+  }, [navigation, headerButtonPressHandler]);
+
 
   return (
     <ScrollView style={styles.rootContainer}>
