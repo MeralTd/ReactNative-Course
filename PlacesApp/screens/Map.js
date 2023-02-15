c8df7ec0a1d062fb52a07e69e39146e69c96be1b
Meral <meraltasdemir77@gmail.com>
2023-02-15 17:15:16 +0300

    allowing-map-interaction

--- PlacesApp/screens/Map.js
+++ PlacesApp/screens/Map.js
@@ -1,15 +1,42 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 function Map() {
-  const region = {
-    latitude: 37.78,
-    longitude: -122.43,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  };
+    const [selectedLocation, setSelectedLocation] = useState();
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+    const region = {
+        latitude: 37.78,
+        longitude: -122.43,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    };
+
+    function selectLocationHandler(event){
+        const lat = event.nativeEvent.coordinate.latitude;
+        const lng = event.nativeEvent.coordinate.longitude;
+
+        setSelectedLocation({lat: lat, lng: lng});
+    }
+
+    return (
+        <MapView
+            style={styles.map}
+            initialRegion={region}
+            onPress={selectLocationHandler}
+        >
+            {selectedLocation && (
+                <Marker
+                    title="Picked Location"
+                    coordinate={{
+                        latitude: selectedLocation.lat,
+                        longitude: selectedLocation.lng,
+                    }}
+                />
+            )}
+        </MapView>
+    )
+    
 }
 
 export default Map;
